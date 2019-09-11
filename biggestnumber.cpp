93db1e4a19245438590359daa17ce7d92c5cebaa
adgilbert@mail.csuchico.edu <adgilbert@ecc-jaguar.csuchico.edu>
2019-09-11 16:54:34 -0700

    this let you add numbers but not doesnt tell you which one is bigger

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,18 +1,28 @@
-//Author:
+//Author:Andrew Gilbert
 #include<iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
 
 int main()
 {
   //declare variables here
+    int first = 0;
+    int second = 0;
+   
+
 
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
-
+    std::cin>>first;    
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
 
   //determine if first or second is bigger and store that in a third variable
-
+    std::cin>>second;
 
   std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
